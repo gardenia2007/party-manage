0480aec75f100f8c942972fc87068a3164fca95e
李洪祥 <gardeniaxy@gmail.com>
2014-02-12 12:13:42 +0800

    完成客户端获取、更新信息的接口

--- controllers/mobile.py
+++ controllers/mobile.py
@@ -13,25 +13,25 @@ db = web.config._db
 
 
 require = [
-	{'filed':'rdsqs_fz',		'name':'入党申请书',				 'type':'date'},
-	{'filed':'sxhb_fz', 		'name':'思想汇报',				 'type':'count'},
-	{'filed':'kcxs_fz', 		'name':'考察写实',				 'type':'count'},
-	{'filed':'zs_fz', 			'name':'政审',				 	 'type':'date'},
-	{'filed':'xxcjd_fz',		'name':'学习成绩单',				 'type':'date'},
-	{'filed':'bjyj_fz',		 	'name':'班级意见',				 'type':'date'},
-	{'filed':'bzryj_fz',		'name':'班主任意见',				 'type':'date'},
-	{'filed':'tzbyj_fz', 		'name':'团支部意见',				 'type':'date'},
-	{'filed':'dxjyzs_fz',		'name':'党校结业证',				 'type':'date'},
-	{'filed':'zz_fz',		 	'name':'自传',				 	 'type':'date'},
-	{'filed':'rdzys_fz',		'name':'入党志愿书',				 'type':'date'},
-	{'filed':'fzpj_fz',		 	'name':'发展票决',				 'type':'date'},
-	{'filed':'sxhb_yb',		 	'name':'思想汇报',				 'type':'count'},
-	{'filed':'kcxs_yb', 		'name':'考察写实',				 'type':'count'},
-	{'filed':'dxjyzs_yb',		'name':'党校结业证',				 'type':'date'},
-	{'filed':'bnzj_yb', 		'name':'半年总结',				 'type':'date'},
-	{'filed':'qnzj_yb',			'name':'全年总结',				 'type':'date'},
-	{'filed':'zzsq_yb',			'name':'转正申请',				 'type':'date'},
-	{'filed':'zzpj_yb', 		'name':'转正票决',				 'type':'date'},
+	{'field':'rdsqs_fz',		'name':'入党申请书',				 'type':'date'},
+	{'field':'sxhb_fz', 		'name':'思想汇报',				 'type':'count'},
+	{'field':'kcxs_fz', 		'name':'考察写实',				 'type':'count'},
+	{'field':'zs_fz', 			'name':'政审',				 	 'type':'date'},
+	{'field':'xxcjd_fz',		'name':'学习成绩单',				 'type':'date'},
+	{'field':'bjyj_fz',		 	'name':'班级意见',				 'type':'date'},
+	{'field':'bzryj_fz',		'name':'班主任意见',				 'type':'date'},
+	{'field':'tzbyj_fz', 		'name':'团支部意见',				 'type':'date'},
+	{'field':'dxjyzs_fz',		'name':'党校结业证',				 'type':'date'},
+	{'field':'zz_fz',		 	'name':'自传',				 	 'type':'date'},
+	{'field':'rdzys_fz',		'name':'入党志愿书',				 'type':'date'},
+	{'field':'fzpj_fz',		 	'name':'发展票决',				 'type':'date'},
+	{'field':'sxhb_yb',		 	'name':'思想汇报',				 'type':'count'},
+	{'field':'kcxs_yb', 		'name':'考察写实',				 'type':'count'},
+	{'field':'dxjyzs_yb',		'name':'党校结业证',				 'type':'date'},
+	{'field':'bnzj_yb', 		'name':'半年总结',				 'type':'date'},
+	{'field':'qnzj_yb',			'name':'全年总结',				 'type':'date'},
+	{'field':'zzsq_yb',			'name':'转正申请',				 'type':'date'},
+	{'field':'zzpj_yb', 		'name':'转正票决',				 'type':'date'},
 ]
 
 
@@ -40,28 +40,46 @@ class Index:
 		return "hello!"
 	def POST(self):
 		pass
- 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	
+
+
 class InfoAll:
 	def GET(self, uid):
 		try:
-			user = db.select('user', {'xh':uid}, where="xh = $xh")[0]
-			data = []
+			user = list(db.select('user', {'xh':uid}, where="xh = $xh"))
+			if len(user) == 0:
+				return
+			data, user = [], user[0]
 			for item in require:
-				v = user.get(item['filed'])
+				v = user.get(item['field'])
 				if v is not None: value = str(v)
-				else: value = None
+				else: value = ''
 				data.append(dict(item, **{'value':value}))
 			return json.dumps(data)
 		except Exception, e:
 			raise e
-	def POST(self):
+	def POST(self, uid):
 		pass
 
 class InfoUpdate:
-	def GET(self):
-		pass
+	def GET(self, uid):
+		return self.POST('1100300220')
 	def POST(self, uid):
-		pass
+		raw_data = web.data()
+		# raw_data = '[{"field":"rdsqs_fz","name":"入党申请书","value":"1234","type":"date"},{"field":"sxhb_fz","name":"思想汇报","value":"1234","type":"count"}]'
+		try:
+			data = json.loads(raw_data)
+			sql = 'UPDATE user set '
+			for item in data:
+				sql = sql + " %s = '%s' ,"%(item['field'], item['value'])
+			sql =  sql[:-1] + ' where xh = $xh'
+			r = db.query(sql, vars={'xh':uid})
+			print r
+			if r == 0:
+				return json.dumps({'status':-1})
+			else:
+				return json.dumps({'status':0})
+		except Exception, e:
+			raise e
 
 class InfoBasic:
 	def GET(self, uid):
@@ -73,7 +91,7 @@ class InfoBasic:
 		else:
 			d = {'status':True, 'name':user[0]['xm'], 'zb':user[0]['name']}
 		return json.dumps(d)
-	def POST(self):
+	def POST(self, uid):
 		pass
 
 
