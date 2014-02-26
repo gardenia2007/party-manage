473a3a4da1de0502a245a19011391f7e3f313ddc
李洪祥 <gardeniaxy@gmail.com>
2014-02-26 21:18:38 +0800

    修复应交材料份数bug

--- controllers/auth.py
+++ controllers/auth.py
@@ -26,8 +26,8 @@ class User:
 		return web.config._render.success_page(self.session, '', msg)
 	def get_all_info(self, user):
 		report = Report()
-		fz_expect_num = report.get_quater_num(user['qdjjfz_sj'])
-		yb_expect_num = report.get_quater_num(user['rdsj_sj'])
+		fz_expect_num = report.get_quater_num(user['qdjjfz_sj'], user['rdsj_sj'])
+		yb_expect_num = report.get_quater_num(user['rdsj_sj'], user['zzsj_sj'])
 		d = {'expect_sxhb_fz':fz_expect_num, 'expect_kcxs_fz':fz_expect_num,
 			 'expect_sxhb_yb':yb_expect_num, 'expect_kcxs_yb':yb_expect_num}
 		new_user = dict(user, **d)
@@ -36,13 +36,14 @@ class User:
 class Report:
 	def __init__(self):
 		pass
-	def get_quater_num(self, begin):
+	def get_quater_num(self, begin, end):
 		if begin is None or begin == '':
 			return 0
-		today = datetime.date.today()
-		# 从begin到today的总季度数
-		total_quater = ( today.year - begin.year ) * 4 \
-			+ ( self.quater(today) - self.quater(begin))
+		if end is None or end == '':
+			end = datetime.date.today()
+		# 从begin到end总季度数
+		total_quater = ( end.year - begin.year ) * 4 \
+			+ ( self.quater(end) - self.quater(begin))
 		return total_quater
 	def quater(self, d): # 对应的季度
 		return d.month / 4
